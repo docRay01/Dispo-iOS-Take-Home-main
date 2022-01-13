f4aefb7f957c4d69ca1ce64f59738fc4121aae80
Davis, Steven <R.Steven.Davis@disney.com>
2022-01-13 00:12:59 -0500

    Add preview image to APIListResponse

--- Dispo Take Home/Util/APIListResponse.swift
+++ Dispo Take Home/Util/APIListResponse.swift
@@ -1,29 +1,30 @@
 import UIKit
 
 struct APIListResponse: Codable {
-  var data: [GifObject]
+    var data: [GifObject]
 }
 
 struct APIGifInfoResponse: Codable {
-  var data: GifObject
+    var data: GifObject
 }
 
 struct GifObject: Codable {
-  var id: String
-  var title: String
-  var source_tld: String
-  var rating: String
-  /// Giphy URL (not gif url to be displayed)
-  var url: URL
-  var images: Images
-
-  struct Images: Codable {
-    var fixed_height: Image
-
-    struct Image: Codable {
-      var url: URL
-      var width: String
-      var height: String
+    var id: String
+    var title: String
+    var source_tld: String
+    var rating: String
+    /// Giphy URL (not gif url to be displayed)
+    var url: URL
+    var images: Images
+    
+    struct Images: Codable {
+        var fixed_height: Image
+        var fixed_height_small: Image
+        
+        struct Image: Codable {
+            var url: URL
+            var width: String
+            var height: String
+        }
     }
-  }
 }
