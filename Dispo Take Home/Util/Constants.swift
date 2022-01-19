450772f4e651f751d961fd7d72e49ee1ad183e35
Davis, Steven <R.Steven.Davis@disney.com>
2022-01-18 20:32:58 -0500

    Add a comment to constants file

--- Dispo Take Home/Util/Constants.swift
+++ Dispo Take Home/Util/Constants.swift
@@ -1,6 +1,9 @@
 enum Constants {}
 
 extension Constants {
-  // Get an API key from https://developers.giphy.com/dashboard/
-  static let giphyApiKey = ""
+    // Get an API key from https://developers.giphy.com/dashboard/
+    
+    // First rule of public GitHub repos - never include your API key in the repo.
+    // See AppSecret.json
+    static let giphyApiKey = "Not included in this repo"
 }
